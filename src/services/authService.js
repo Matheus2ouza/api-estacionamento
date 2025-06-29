2d4985bd61e38354ad557e5ead8c9a3581b19b3c
matheus2ouza <matheusfurtadogg@gmail.com>
2025-06-29 06:00:58 -0300

    Fix import path for PrismaClient in authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('../generated/prisma');
 const prisma = new PrismaClient();
 const { createHash, verifyPassword } = require('../utils/authUtils');
 const jwt = require('jsonwebtoken');
