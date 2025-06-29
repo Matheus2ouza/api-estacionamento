f69b57aa35fd0b69b8a240b048cdd1009820cb8f
matheus2ouza <matheusfurtadogg@gmail.com>
2025-06-29 05:29:18 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Bem-vindo à minha API! ❤️');
+});
+
+// Exportação para a Vercel
+const server = require('http').createServer(app);
+module.exports = (req, res) => {
+    return app(req, res); // Vercel precisa dessa função handler
+};
