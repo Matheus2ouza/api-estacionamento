1c91e170e30f0d1e18622b1183bc9ddcf334be4a
matheus2ouza <matheusfurtadogg@gmail.com>
2025-06-29 05:34:48 -0300

    Add static serving for favicon.ico

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/favicon.ico', express.static(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.get('/', (req, res) => {
     res.send('Bem-vindo à minha API! ❤️');
 });
