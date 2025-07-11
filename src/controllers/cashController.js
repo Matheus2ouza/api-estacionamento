00f8ba855f79373287ce8bd2990e01c6758ae9ec
matheus2ouza <matheusfurtadogg@gmail.com>
2025-07-10 23:42:24 -0300

    ajuste para verificar dados

--- src/controllers/cashController.js
+++ src/controllers/cashController.js
@@ -7,11 +7,15 @@ exports.statusCash = async (req, res) => {
   try{
     const date = DateTime.now().setZone("America/Belem").toJSDate();
     const date2 = DateTime.now().setZone("America/Belem").toJSDate();
+    const date3 = new Date();
 
     console.log(`Data com toJSDate: ${date}`);
     console.log(`Data sem toJSDate: ${date2}`);
+    console.log(`Data com o new date: ${date3}`);
   
-
+    return res.status(201).json({
+      success: true
+    })
 
   } catch (error) {
     console.log(`[CashController] Erro ao tentar buscar o status do caixa: ${error}`);
