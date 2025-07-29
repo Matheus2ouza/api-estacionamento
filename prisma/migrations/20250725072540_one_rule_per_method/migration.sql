d8da04fa1ea18b7023600fb820e782a645369147
matheus2ouza <matheusfurtadogg@gmail.com>
2025-07-28 22:55:13 -0300

    novo modelo prisma

--- prisma/migrations/20250725072540_one_rule_per_method/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[billingMethodId]` on the table `billing_rule` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "billing_rule_billingMethodId_key" ON "billing_rule"("billingMethodId");
--- prisma/migrations/20250725080245_reformulando/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "billing_rule_billingMethodId_key";
