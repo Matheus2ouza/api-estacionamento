ad2e358d57de2b9b8f9b3f33b5567f4deca53b6d
matheus2ouza <matheusfurtadogg@gmail.com>
2025-07-31 13:33:55 -0300

    ajuste para rodar localmente

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Middleware
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, 'src', 'public', 'img', 'ico')));
 
+// Rotas de abertura
 app.get('/', (req, res) => {
     res.send('Bem-vindo à minha API! ❤️');
 });
 
+//Rotas
 const authRoutes = require('./src/routes/authRoutes');
 const vehicleRoutes = require('./src/routes/vehicleRoutes');
 const cashRoutes = require('./src/routes/cashRoutes');
@@ -17,11 +19,15 @@ const productRoutes = require('./src/routes/productRoutes');
 
 app.use('/auth', authRoutes);
 app.use('/vehicles', vehicleRoutes);
-app.use('/cash', cashRoutes)
-app.use('/products', productRoutes)
+app.use('/cash', cashRoutes);
+app.use('/products', productRoutes);
 
-// Exportação para a Vercel
-const server = require('http').createServer(app);
-module.exports = (req, res) => {
-    return app(req, res);
-};
+if (require.main === module) {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando em http://localhost:${PORT}`);
+    });
+}
+
+// Exporta para Vercel
+module.exports = app;
